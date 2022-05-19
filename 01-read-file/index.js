fee6b976b82d227d25d627141e2b689dfdfaf0d5
Stanislav Kolpakov <kolpakoff@gmail.com>
2022-05-19 21:56:49 +0200

    feat: subtask 01-read-file completed

---
+++ 01-read-file/index.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+const { stdout } = process;
+
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'));
+ 
+stream.on('readable', function(){
+    const data = stream.read();
+    if (data != null) stdout.write(data.toString());
+});
+ 
